e63fa3f302b314759614b3320b98b563dd32f8b5
hasanaydin0 <84991976+hasanaydin0@users.noreply.github.com>
2021-06-29 18:49:47 +0300

    SqLiteSave

--- app/src/main/java/com/hasanaydin/travelbook/MapsActivity.kt
+++ app/src/main/java/com/hasanaydin/travelbook/MapsActivity.kt
@@ -131,13 +131,31 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
                 mMap.addMarker(MarkerOptions().position(p0).title(address))
 
+                val newPlace = Place(address,p0.latitude,p0.longitude)
+
                 val dialog = AlertDialog.Builder(this@MapsActivity)
                 dialog.setCancelable(false)
                 dialog.setTitle("Are You Sure")
-                dialog.setMessage(address)
+                dialog.setMessage(newPlace.address)
                 dialog.setPositiveButton("Yes"){ dialog, which ->
-
                     // SQLite Save
+                    try {
+
+                        val database = openOrCreateDatabase("Places",Context.MODE_PRIVATE,null)
+                        database.execSQL("CREATE TABLE IF NOT EXISTS places(address VARCHAR, latitude DOUBLE, longitude DOUBLE)")
+                        val toCompile = "INSERT INTO places (address,latitude,longitude) VALUES (?,?,?)"
+                        val sqLiteStatement = database.compileStatement(toCompile)
+                        sqLiteStatement.bindString(1,newPlace.address)
+                        sqLiteStatement.bindDouble(2,newPlace.latitude!!)
+                        sqLiteStatement.bindDouble(3,newPlace.longitude!!)
+                        sqLiteStatement.execute()
+
+                    }catch (e:Exception){
+                        e.printStackTrace()
+                    }
+
+                    Toast.makeText(this@MapsActivity, "New Place Created", Toast.LENGTH_LONG).show()
+
 
                 }.setNegativeButton("No",) { dialog, which ->
                     Toast.makeText(this@MapsActivity, "Canceled!", Toast.LENGTH_LONG).show()
---
+++ app/src/main/java/com/hasanaydin/travelbook/Place.kt
@@ -0,0 +1,7 @@
+package com.hasanaydin.travelbook
+
+class Place(var address : String?, var latitude : Double?, var longitude : Double?) {
+
+
+
+}
