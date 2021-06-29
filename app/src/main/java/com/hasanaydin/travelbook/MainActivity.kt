27ef4324534052b0dac668f6a7df6eaa83b761e2
hasanaydin0 <84991976+hasanaydin0@users.noreply.github.com>
2021-06-29 15:22:24 +0300

    OptionsMenu added

--- app/src/main/java/com/hasanaydin/travelbook/MainActivity.kt
+++ app/src/main/java/com/hasanaydin/travelbook/MainActivity.kt
@@ -1,11 +1,46 @@
 package com.hasanaydin.travelbook
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import com.hasanaydin.travelbook.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+
+    // OptionsMenu
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+        val menuInflater = menuInflater
+        menuInflater.inflate(R.menu.add_place,menu)
+
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        if (item.itemId == R.id.add_place_option){
+
+            val intent = Intent(applicationContext,MapsActivity::class.java)
+            startActivity(intent)
+
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+    // End OptionsMenu
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
 }
